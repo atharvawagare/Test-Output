f13bc05b38c24d83bb9cee9f0852b6c622abf719
Atharva Wagare <52174494+atharvawagare@users.noreply.github.com>
2022-12-02 16:47:03 +0530

    Update Detector.py

--- Detector.py
+++ Detector.py
@@ -7,16 +7,14 @@ from detectron2 import model_zoo
 import cv2
 import numpy as np
 
-
-
 class Detector:
     def __init__(self, model_type = "OD"):
         self.cfg = get_cfg()
         self.model_type = model_type
         # Load model config and pretrained model
         if model_type == "OD" # Object Detection
-            self.cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml"))
-            self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml")
+            self.cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml"))
+            self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml")
         elif model_type == "IS" # Instance Segmentation
             self.cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
             self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/faster_rcnn_R_101_FPN_3x.yaml")
@@ -30,7 +28,6 @@ class Detector:
             self.cfg.merge_from_file(model_zoo.get_config_file("COCO-PanopticSegmentation/panoptic_fpn_R_101_3x.yaml"))
             self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-PanopticSegmentation/panoptic_fpn_R_101_3x.yaml")
         
-
         self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7
         self.cfg.MODEL.DEVICE = "cuda" #cpu or cuda
 
@@ -51,3 +48,23 @@ class Detector:
         #cv2.imshow("Result", output.get_image()[:,:,::-1])
         cv2.imwrite("Result.jpg", output.get_image()[:,:,::-1])
         #cv2.waitkey(0)
+        
+    '''def onVideo(self, videoPath):
+        cap = cv2.VideoCapture(videoPath)
+        
+        if (cap.usOpened()==False):
+            print("Error opening the file ...")
+            return
+        
+        (succes, image) = cap.read()
+        
+        while succes:
+            if self.model_type != "PS":
+                predictions = self.predictor(image)
+
+                viz = Visualizer(image[:,:,::-1], metadata = MetadataCatalog.get(self.cfg.DATASETS.TRAIN[0]), instance_mode = ColorMode.IMAGE_BW)
+                output = viz.draw_instance_predictions(predictions["instances"].to("cpu"))
+            else:
+                predictions, segmentInfo = self.predictor(image)["panoptic_seg"]
+                viz = Visualizer(image[:,:,::-1], MetadataCatalog.get(self.cfg.DATASETS.TRAIN[0]))
+                output = viz.draw_panoptic_seg_predictions(predictions.to("cpu"), segmentInfo)'''
