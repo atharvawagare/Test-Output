8ef0e70ba4242a626692f09960dd053111fa2818
brain <brain@gpunode-enp2s0f0>
2022-12-02 17:53:28 +0530

    Detector.py

--- Detector.py
+++ Detector.py
@@ -12,19 +12,19 @@ class Detector:
         self.cfg = get_cfg()
         self.model_type = model_type
         # Load model config and pretrained model
-        if model_type == "OD" # Object Detection
+        if model_type == "OD": # Object Detection
             self.cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml"))
             self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml")
-        elif model_type == "IS" # Instance Segmentation
+        elif model_type == "IS": # Instance Segmentation
             self.cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
             self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/faster_rcnn_R_101_FPN_3x.yaml")
-        elif model_type == "KP" # Keypoint Detection
+        elif model_type == "KP": # Keypoint Detection
             self.cfg.merge_from_file(model_zoo.get_config_file("COCO-Keypoints/keypoint_rcnn_R_50_FPN_3x.yaml"))
             self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Keypoints/keypoint_rcnn_R_50_FPN_3x.yaml")
-        elif model_type == "LVIS" # LVIS Segmentation
+        elif model_type == "LVIS": # LVIS Segmentation
             self.cfg.merge_from_file(model_zoo.get_config_file("LVISv0.5-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_1x.yaml"))
             self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("LVISv0.5-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_1x.yaml")
-        elif model_type == "PS" # Panoptic Segmentation
+        elif model_type == "PS": # Panoptic Segmentation
             self.cfg.merge_from_file(model_zoo.get_config_file("COCO-PanopticSegmentation/panoptic_fpn_R_101_3x.yaml"))
             self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-PanopticSegmentation/panoptic_fpn_R_101_3x.yaml")
         
