3266b4630d7f8d2861d5e5518571cfff88227c58
Atharva Wagare <52174494+atharvawagare@users.noreply.github.com>
2022-12-02 16:20:55 +0530

    Update Detector.py

--- Detector.py
+++ Detector.py
@@ -10,14 +10,26 @@ import numpy as np
 
 
 class Detector:
-    def __init__(self):
+    def __init__(self, model_type = "OD"):
         self.cfg = get_cfg()
+        self.model_type = model_type
         # Load model config and pretrained model
-        #self.cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml"))
-        #self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml")
-
-        self.cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
-        self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/faster_rcnn_R_101_FPN_3x.yaml")
+        if model_type == "OD" # Object Detection
+            self.cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml"))
+            self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml")
+        elif model_type == "IS" # Instance Segmentation
+            self.cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+            self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/faster_rcnn_R_101_FPN_3x.yaml")
+        elif model_type == "KP" # Keypoint Detection
+            self.cfg.merge_from_file(model_zoo.get_config_file("COCO-Keypoints/keypoint_rcnn_R_50_FPN_3x.yaml"))
+            self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Keypoints/keypoint_rcnn_R_50_FPN_3x.yaml")
+        elif model_type == "LVIS" # LVIS Segmentation
+            self.cfg.merge_from_file(model_zoo.get_config_file("LVISv0.5-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_1x.yaml"))
+            self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("LVISv0.5-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_1x.yaml")
+        elif model_type == "PS" # Panoptic Segmentation
+            self.cfg.merge_from_file(model_zoo.get_config_file("COCO-PanopticSegmentation/panoptic_fpn_R_101_3x.yaml"))
+            self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-PanopticSegmentation/panoptic_fpn_R_101_3x.yaml")
+        
 
         self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7
         self.cfg.MODEL.DEVICE = "cuda" #cpu or cuda
@@ -26,10 +38,15 @@ class Detector:
 
     def onImage(self, imagePath):
         image = cv2.imread(imagePath)
-        predictions = self.predictor(image)
+        if self.model_type != "PS":
+            predictions = self.predictor(image)
 
-        viz = Visualizer(image[:,:,::-1], metadata = MetadataCatalog.get(self.cfg.DATASETS.TRAIN[0]), instance_mode = ColorMode.IMAGE_BW)
-        output = viz.draw_instance_predictions(predictions["instances"].to("cpu"))
+            viz = Visualizer(image[:,:,::-1], metadata = MetadataCatalog.get(self.cfg.DATASETS.TRAIN[0]), instance_mode = ColorMode.IMAGE_BW)
+            output = viz.draw_instance_predictions(predictions["instances"].to("cpu"))
+        else:
+            predictions, segmentInfo = self.predictor(image)["panoptic_seg"]
+            viz = Visualizer(image[:,:,::-1], MetadataCatalog.get(self.cfg.DATASETS.TRAIN[0]))
+            output = viz.draw_panoptic_seg_predictions(predictions.to("cpu"), segmentInfo)
 
         #cv2.imshow("Result", output.get_image()[:,:,::-1])
         cv2.imwrite("Result.jpg", output.get_image()[:,:,::-1])
