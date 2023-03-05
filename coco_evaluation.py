85952702e9ff61f43445be5588a4b2c008ea2a17
atharvawagare <brain@gpunode-enp2s0f0>
2023-03-05 17:38:00 +0530

    Testing Outputs of COCO-InstanceSegmentation

--- coco_evaluation.py
+++ coco_evaluation.py
@@ -6,15 +6,16 @@ from detectron2.data.datasets import register_coco_instances
 from detectron2.evaluation import COCOEvaluator, inference_on_dataset
 
 #register your data
-register_coco_instances("my_dataset_train", {}, "/code/detectron2/detectron2/instances_train2017.json", "/code/detectron2/detectron2/train2017")
-register_coco_instances("my_dataset_val", {}, "/code/detectron2/detectron2/instances_val2017.json", "/code/detectron2/detectron2/val2017")
-register_coco_instances("my_dataset_test", {}, "/code/detectron2/detectron2/instances_test2017.json", "/code/detectron2/detectron2/test2017")
+#register_coco_instances("my_dataset_train", {}, "./detectron2/coco/annotations/instances_train2017.json", "./detectron2/coco/train2017")
+#register_coco_instances("my_dataset_val", {}, "./detectron2/coco/annotations/instances_val2017.json", "./detectron2/coco/val2017")
+#register_coco_instances("my_dataset_test", {}, "./detectron2/coco/annotations/instances_val2017_100.json", "./detectron2/coco/val2017")
+register_coco_instances("my_dataset_test", {}, "./datasets/coco/annotations/instances_val2017_100.json", "./datasets/coco/val2017")
 
 #load the config file, configure the threshold value, load weights 
 cfg = get_cfg()
 cfg.merge_from_file("./configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
 cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
-cfg.MODEL.WEIGHTS = "$FVCORE_CACHE/model_final_f10217.pkl"
+cfg.MODEL.WEIGHTS = "/root/.torch/iopath_cache/detectron2/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x/137849600/model_final_f10217.pkl"
 
 # Create predictor
 predictor = DefaultPredictor(cfg)
